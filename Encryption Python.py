aafaf8626fdf386e9d64e5210e576853e95dbe2b
DogeDude22 <>
2020-08-20 18:46:00 -0700

    Version 0.2: Added single word support

--- Encryption Python.py
+++ Encryption Python.py
@@ -3,21 +3,17 @@ alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r
 encrypted_alphabet=['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
 counter=0
 is_letter=0
-
+encrypted_word = []
 
 #Asks the user for the letter
-letter=input("enter your first letter")
+base_word = input("Enter your first word: ")
 
 #Figures out the letter and what to make it
-if is_letter==0:
-    for i in range(26):
-        if letter==alphabet[i]:
-            is_letter==1
-            counter=i
-            
-#This prints out the encrypted letter
-print(encrypted_alphabet[counter])
+word = [letter for letter in base_word]
+print(word)
 
 
+for x in range(len(word)):
+    encrypted_word.append(encrypted_alphabet[alphabet.index(word[x])])
 
-
+print(encrypted_word)
---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple caesar cypher started by me in 2019 as an idea on a vacation. 
+
