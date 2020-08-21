753ff65e9a4bfa4b55b8ac6dd9dfe7b9577ce2ad
DogeDude22 <>
2020-08-20 19:45:09 -0700

    Version 1.0 Added uppercase patch and updated README

--- Encryption Python.py
+++ Encryption Python.py
@@ -1,20 +1,15 @@
-#This is the list for the alphabet,the encrypted alphabet, and the counter that tells the encrypted list which letter to use.
-alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' ']
-encrypted_alphabet=['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m', ' ']
-counter=0
-is_letter=0
+alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' ']
+encrypted_alphabet = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m', ' ']
+
 encrypted_word = []
 finalword = ''
 
-#Asks the user for the input
 base_word = input("Enter what you want to encrypt: ")
+base_word = base_word.lower()
 
-#Figures out the letter and what to make it
 word = [letter for letter in base_word]
 
 for x in range(len(word)):
     encrypted_word.append(encrypted_alphabet[alphabet.index(word[x])])
 
 print(finalword.join(encrypted_word))
-
-#TODO: Add .env file
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Encryption Project
 
 This is a simple Caesar Cypher started by me in 2019 as an idea on a vacation. 🌴
-This was before I had even heard of the Caesar Cypher.
+This was before I had even heard of the Caesar Cypher. 💡
+
 
